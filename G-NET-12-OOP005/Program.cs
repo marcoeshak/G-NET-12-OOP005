473bee6189ca6f133bbe2a0f32b5f9c60179f2c8
marco <marco@DESKTOP-GCUIOA1>
2026-03-11 01:55:52 +0200

    Q 01

--- G-NET-12-OOP005/Program.cs
+++ G-NET-12-OOP005/Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            #region Part 01
+
+            #region Q 01
+
+            /*
+             * An interface in C# defines a contract that a class must follow. It specifies what a class can do, not how it does it. Any class that implements the interface must provide the implementation for all its members.
+
+             * We use interfaces instead of depending on concrete classes to avoid tight coupling and to make the code depend on abstraction rather than a specific implementation.
+
+             * Benefits of using interfaces:
+
+             * Loose coupling between classes.
+
+             * Flexibility and easy extension by allowing multiple implementations.
+
+             * Easier testing, especially when using mock implementations in unit tests.
+             *
+             */
+
+            #endregion
+
+
+
+
+
+
+            #endregion
+
+
+
+
         }
     }
 }
